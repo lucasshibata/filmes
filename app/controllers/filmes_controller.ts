68a964f5562d247ae04d97377e51e72c5e357980
Lucas Shibata <lucaspish@gmail.com>
2026-01-06 11:06:57 -0300

    finalizando os metodos delete e update

--- app/controllers/filmes_controller.ts
+++ app/controllers/filmes_controller.ts
@@ -7,12 +7,29 @@ export default class FilmesController {
         return Filme.query()
     }
 
-    show(){
-
+    show({params}:HttpContext){
+        const id = params.id
+        return Filme.findOrFail(id)
     }
 
     async store({request}:HttpContext){
         const dados = request.only(['nome', 'classificacao', 'sinopse', 'duracao', 'categoria_id'])
         return await Filme.create(dados)
+    }
+
+    async update({request, params}:HttpContext){
+        const id = params.id
+        const filme = await Filme.findOrFail(id)
+        const dados = request.only(['nome', 'classificacao', 'sinopse', 'duracao', 'categoria_id']);
+        filme.merge(dados)
+        await filme.save()
+        return filme
+    }
+
+    async destroy({params}:HttpContext){
+        const id = params.id
+        const filme = await Filme.findOrFail(id)
+        filme.delete()
+        return {sucess:true}
     }
 }
